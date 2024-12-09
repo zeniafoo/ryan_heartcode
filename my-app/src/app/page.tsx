67b7c500a70654a625bcadaed329c97520362d7e
zeniafoo <zenia.foo.2023@scis.smu.edu.sg>
2024-12-09 16:37:59 +0800

    meow

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import { motion } from "framer-motion";
+import React from "react";
+import { AuroraBackground } from "../components/ui/aurora-background";
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+      >
+        <div className="relative flex flex-col gap-4 items-center justify-center px-4">
+
+
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          Escape the fake high, Reach real heights
+        </div>
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+          Hi im Ryan
+        </div>
+        </div>
+      </motion.div>
+    </AuroraBackground>
   );
 }
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,18 +1,47 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+const {
+	default: flattenColorPalette,
+  } = require("tailwindcss/lib/util/flattenColorPalette");
+  
+  /** @type {import('tailwindcss').Config} */
+  module.exports = {
+	content: [
+	  // your paths
+	  "./src/**/*.{html,js,jsx,ts,tsx}",
+	],
+	darkMode: "class",
+	theme: {
+	  extend: {
+		colors: {
+			background: 'var(--background)', // Use your CSS variables here
+			foreground: 'var(--foreground)',
+		  },
+		animation: {
+		  aurora: "aurora 60s linear infinite",
+		},
+		keyframes: {
+		  aurora: {
+			from: {
+			  backgroundPosition: "50% 50%, 50% 50%",
+			},
+			to: {
+			  backgroundPosition: "350% 50%, 350% 50%",
+			},
+		  },
+		},
+	  },
+	},
+	plugins: [addVariablesForColors],
+  };
+  
+  // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+  function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+  
+	addBase({
+	  ":root": newVars,
+	});
+  }
+  
