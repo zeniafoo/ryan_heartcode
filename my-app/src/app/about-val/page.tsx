e25d7eddbe0e52e810fc6b0de470c3c906259883
valerie0307 <valerietan2009@gmail.com>
2024-12-09 17:23:01 +0800

    val update

--- my-app/src/app/about-val/page.tsx
+++ my-app/src/app/about-val/page.tsx
@@ -1,7 +1,15 @@
-export default function aboutval(){
-    return(
-        <div>
-            hi i am val
-        </div>
-    )
+export default function Home() {
+  return (
+    <div
+      className="bg-cover bg-center h-screen"
+      style={{
+        backgroundImage: `url("/heartcodebackground.png")`,
+        backgroundPosition: 'center 20%'  // Adjust the value to move the image down
+      }}
+    >
+      <div className="text-white font-bold text-4xl p-4">
+        Drugs are BAD BAD BAD
+      </div>
+    </div>
+  );
 }
