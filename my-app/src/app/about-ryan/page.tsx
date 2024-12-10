3384e279b47295328791ac7175aa3cf047680241
zeniafoo <zenia.foo.2023@scis.smu.edu.sg>
2024-12-10 17:32:29 +0800

    idk what changed

--- my-app/src/app/about-ryan/page.tsx
+++ my-app/src/app/about-ryan/page.tsx
@@ -105,7 +105,8 @@ export default function aboutryan(){
             </LampContainer>
             <div className="py-20">
                     <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
-                        Get to know your iSad.
+                        Get to know my sport
+                        
                     </h2>
                     <Carousel items={cards} />
                     </div>
