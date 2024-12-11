18e66e8b5625c577c07a6e0e0569e05823d3f3cc
root <root@NathansLaptop.>
2024-12-11 10:23:52 +0800

    clerk stuff added to quiz

--- my-app/src/app/quiz/page.tsx
+++ my-app/src/app/quiz/page.tsx
@@ -8,26 +8,27 @@ import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/hooks/use-toast";
+import { insertOneUser } from "@/server/user";
 
 const FormSchema = z.object({
-    name: z.string({
-        required_error: "Please enter a name"
-    }).min(2, {
-        message: "name must be more than 2 characters long"
-    }).max(20, {
-        message: "name must be no longer than 20 characters"
-    }),
-    question1: z.string({
-        required_error: "Please select an option"
-    })
+	name: z.string({
+    	required_error: "Please enter a name"
+	}).min(2, {
+    	message: "name must be more than 2 characters long"
+	}).max(20, {
+    	message: "name must be no longer than 20 characters"
+	}),
+	question1: z.string({
+    	required_error: "Please select an option"
+	})
 })
 
 export default function Quiz() {
-    const { toast } = useToast();
+	const { toast } = useToast();
 
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema)
-    })
+	const form = useForm<z.infer<typeof FormSchema>>({
+    	resolver: zodResolver(FormSchema)
+	})
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
     if (data.question1 === "yes") {
@@ -41,49 +42,53 @@ export default function Quiz() {
             description: "Unfortunately you are not a drug dealer",
         })
     }
+
+	const isDrugDealer = true ? data.question1 === "yes" : false;
+	await insertOneUser(data.name, isDrugDealer);
   }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w2/3 space-y-6">
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Question 1:</FormLabel>
-                            <FormDescription>What is your name?</FormDescription>
-                                <FormControl>
-                                    <Input placeholder="your name here" {...field}/>
-                                </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="question1"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Question 2:</FormLabel>
-                            <FormDescription>Do you sell drugs?</FormDescription>
-                            <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                <FormControl>
-                                    <SelectTrigger>
-                                        <SelectValue placeholder="Please select an answer"/>
-                                    </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                    <SelectItem value="yes">Yes</SelectItem>
-                                    <SelectItem value="no">No</SelectItem>
-                                </SelectContent>
-                            </Select>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
-    )
+
+	return (
+    	<Form {...form}>
+        	<form onSubmit={form.handleSubmit(onSubmit)} className="w2/3 space-y-6">
+            	<FormField
+                	control={form.control}
+                	name="name"
+                	render={({ field }) => (
+                    	<FormItem>
+                        	<FormLabel>Question 1:</FormLabel>
+                        	<FormDescription>What is your name?</FormDescription>
+                            	<FormControl>
+                                	<Input placeholder="your name here" {...field}/>
+                            	</FormControl>
+                        	<FormMessage/>
+                    	</FormItem>
+                	)}
+            	/>
+            	<FormField
+                	control={form.control}
+                	name="question1"
+                	render={({ field }) => (
+                    	<FormItem>
+                        	<FormLabel>Question 2:</FormLabel>
+                        	<FormDescription>Do you sell drugs?</FormDescription>
+                        	<Select onValueChange={field.onChange} defaultValue={field.value}>
+                            	<FormControl>
+                                	<SelectTrigger>
+                                    	<SelectValue placeholder="Please select an answer"/>
+                                	</SelectTrigger>
+                            	</FormControl>
+                            	<SelectContent>
+                                	<SelectItem value="yes">Yes</SelectItem>
+                                	<SelectItem value="no">No</SelectItem>
+                            	</SelectContent>
+                        	</Select>
+                        	<FormMessage/>
+                    	</FormItem>
+                	)}
+            	/>
+            	<Button type="submit">Submit</Button>
+        	</form>
+    	</Form>
+	)
 }
