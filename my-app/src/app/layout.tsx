0b4420e744c40fa25b3421c52574d55a8672314c
zeniafoo <zenia.foo.2023@scis.smu.edu.sg>
2024-12-11 14:07:39 +0800

    lol

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
