a3998175a93f11d6801a87e9b2d17a009746cbb6
jiasing101 <nathan.hart.2024@computing.smu.edu.sg>
2024-12-12 17:50:42 +0800

    removed clerk from homepage

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -1,7 +1,7 @@
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
